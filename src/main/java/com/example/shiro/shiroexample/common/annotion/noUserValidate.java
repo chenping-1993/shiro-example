adfbc9d5952cee809fd8e2b29511bbe545d8f202
chenping-1993 <49644614+chenping-1993@users.noreply.github.com>
2020-01-17 15:11:30 +0800

    修改noUserValidate注释信息
    修改noUserValidate注释信息

--- src/main/java/com/example/shiro/shiroexample/common/annotion/noUserValidate.java
+++ src/main/java/com/example/shiro/shiroexample/common/annotion/noUserValidate.java
@@ -8,8 +8,8 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 
 /**
- * 加此注解，不需要校验用户信息，接口方法中不需要加入 AdminUser，
- * 不加此注解，则接口方法中必须加入AdminUser，需要校验登录的用户，获取AdminUser
+ * 加此注解，controller类中的public方法不需要校验用户信息，接口方法中不需要加入 AdminUser，
+ * 不加此注解，则接口public方法中必须加入AdminUser，需要校验登录的用户，获取AdminUser
  * 总之，不想要获取AdminUser，则加此注解；想要获取AdminUser，则不加此注解
  * @Author: chenping
  * @Date: 2019-11-23
